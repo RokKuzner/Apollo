63b0dda90f5d390bce887ad61639c840f15d597c
Rok <rok.ankaran@gmail.com>
2024-09-08 11:37:57 +0200

    created a server side script that runs at 00:01 and retrives last day's music

---
+++ main_server_side.py
@@ -0,0 +1,18 @@
+from apollo import Apollo
+import time
+from datetime import datetime
+import pytz
+
+while True:
+    # Get the current time
+    cest = pytz.timezone("Europe/Berlin")
+    now = datetime.now(cest)
+    
+    # Check if it's 00:01
+    if now.hour == 0 and now.minute == 1:
+        Apollo().run(1)
+        # Wait for 60 seconds to avoid running multiple times within the same minute
+        time.sleep(60)
+    
+    # Sleep for a short while to avoid overloading the CPU
+    time.sleep(30)
